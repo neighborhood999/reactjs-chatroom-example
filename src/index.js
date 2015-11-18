e97b50d864d21f5bfa325757cdc3f186921f423c
Peng-Jie <dean.leehom@gmail.com>
2015-11-18 10:12:02 +0800

    move import css

--- src/index.js
+++ src/index.js
@@ -1,5 +1,8 @@
 import React from 'react';
 import { render } from 'react-dom';
 import App from './components/App';
+import '../assets/css/normalize.css';
+import '../assets/css/reset.css';
+import '../assets/css/style.css';
 
 render(<App />, document.getElementById('root'));
